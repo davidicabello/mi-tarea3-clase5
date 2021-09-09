74013a5a8ea3e74b6ee9f004611cc6c7368a1cd3
davidlp90 <davidivan.eda@gmail.com>
2021-09-09 16:09:28 -0300

    formatting
    formatting

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let totalMinutes = 0;
 let totalSeconds = 0;
 document.querySelector('#button-calc').onclick = function () {
     for (let i = 0; i < $videoHours.length; i++) {
-        totalHours += Number ($videoHours[i].value);
+        totalHours += Number($videoHours[i].value);
         console.log(totalHours);
     }
     document.querySelector('.result').innerText = `La total de tiempo del curso es ${totalHours} Horas,`
